cab9837dbb9536a3349849a7f049e824906984f7
Joshua Yang <joshuayang@mac.com>
2023-05-02 15:10:32 +0800

    Initial Commit

---
+++ RecordVideoIMU/RecordVideoIMUApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordVideoIMUApp.swift
+//  RecordVideoIMU
+//
+//  Created by Joshua Yang on 2023/5/2.
+//
+
+import SwiftUI
+
+@main
+struct RecordVideoIMUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
